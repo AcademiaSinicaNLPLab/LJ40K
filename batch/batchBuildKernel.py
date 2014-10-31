4261cfc0cb0fe98b5555415d9aae4ababc891095
maxis <maxis1718@gmail.com>
2014-10-31 15:55:48 +0800

    Fix missing emotion bug in batchBuildKernel

--- batch/batchBuildKernel.py
+++ batch/batchBuildKernel.py
@@ -33,9 +33,10 @@ if __name__ == '__main__':
     print 'files to be processed:'
     print '\n'.join(['\t'+x for x in to_process])
     print '='*50
-    
+
     for npz_fn in to_process:
 
+        ## rgba_gist+rgba_phog.Xy.happy.train.npz
         print 'processing', npz_fn
 
         rbf = RBF(verbose=True)
@@ -44,12 +45,14 @@ if __name__ == '__main__':
         ## devide data --> train/dev
         X_tr, X_dev = devide(rbf.X, 0.9)
         y_tr, y_dev = devide(rbf.y, 0.9)
-
+        
         ## build
         K_tr, K_dev = rbf.build( (X_tr, X_tr), (X_tr, X_dev) )
 
 
         ## save
+        ## rgba_gist+rgba_phog.Xy.happy.train.npz
+        feature, xy, emotion, dtype, ext = npz_fn.split('.')
         out_train_fn = "%s.Ky.%s.train.npz" % (feature, emotion)
         out_dev_fn = "%s.Ky.%s.dev.npz" % (feature, emotion)
         
