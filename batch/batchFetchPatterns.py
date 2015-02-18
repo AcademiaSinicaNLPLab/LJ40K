fd582d5f524f3dcb97c44368c2b991af03c955c7
doug919 <doug919@gmail.com>
2015-02-19 03:53:08 +0800

    add batch

---
+++ batch/batchFetchPatterns.py
@@ -0,0 +1,25 @@
+import sys
+import logging
+sys.path.append('..')
+from feelit.patterns import PatternFetcher
+
+if __name == '__main__':
+
+	
+    loglevel = logging.DEBUG
+    logging.basicConfig(format='[%(levelname)s] %(message)s', level=loglevel) 
+
+	pf = PatternFetcher(logger=logging)
+
+
+	logging.debug('fetching doc labels')
+	# [(udocId0, emotion0), ...], which is sorted by udocId
+	docs = pf.get_all_doc_labels()
+
+
+	logging.info('forming patterns')
+    for udocID, emotion in docs:
+    	print udocID
+    	print emotion
+
+
